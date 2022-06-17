ae3a873a3b5fe13447ff013f9410da3263ec2905
nhtnr <noahtnr@gmail.com>
2022-06-17 16:19:05 -0400

    add DEPENDENCY_ENVIRONMENT_MARKERS + bump to rc

--- otlet/__init__.py
+++ otlet/__init__.py
@@ -27,5 +27,20 @@ CLI tool and wrapper for the PyPI JSON web API
 from .api import *
 from .exceptions import *
 
-__version__ = "1.0.0a3"
+__version__ = "1.0.0rc1"
 __license__ = "MIT"
+
+DEPENDENCY_ENVIRONMENT_MARKERS = [
+    "os_name",
+    "sys_platform",
+    "platform_machine",
+    "platform_python_implementation",
+    "platform_release",
+    "platform_system",
+    "platform_version",
+    "python_version",
+    "python_full_version",
+    "implementation_name",
+    "implementation_version",
+    "extra"
+]
